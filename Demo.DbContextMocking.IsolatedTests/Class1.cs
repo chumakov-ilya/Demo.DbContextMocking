02c4f8a429df58733c88dfb320c6fa4e97617740
Chumakov Ilya <aidforsoft@mail.ru>
2016-01-25 23:05:37 +0300

    projects setup

--- Demo.DbContextMocking.IsolatedTests/Class1.cs
+++ Demo.DbContextMocking.IsolatedTests/Class1.cs
@@ -3,18 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 
-namespace Demo.DbContextMocking.Tests
+namespace Demo.DbContextMocking.IsolatedTests
 {
     public class Class1
     {
-	    [Test]
-	    public void MyMethod()
-	    {
-		    var repo = new ReportRepository();
-
-
-	    }
     }
 }
