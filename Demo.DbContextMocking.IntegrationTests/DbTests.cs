853fc32feada1aa92bd2c72a810c18087f9bd201
Chumakov Ilya <aidforsoft@mail.ru>
2016-01-26 21:36:06 +0300

    bring untracked files to source control

---
+++ Demo.DbContextMocking.IntegrationTests/DbTests.cs
@@ -0,0 +1,22 @@
+﻿using Demo.DbContextMocking.Entities;
+using Ninject;
+using NUnit.Framework;
+
+namespace Demo.DbContextMocking.IntegrationTests
+{
+	public class DbTests
+	{
+		[Test]
+		public void RealRepository_WhenCalled_ReturnsEmpty()
+		{
+			var kernel = new StandardKernel();
+			kernel.Load<AppModule>();
+
+			var repo = kernel.Get<ReportRepository>();
+
+			var reports = repo.GetReports();
+
+			Assert.IsEmpty(reports);
+		}
+	}
+}
