ee1d61dd04a77cd88df4917241da0d9d3d5f72fd
Chumakov Ilya <aidforsoft@mail.ru>
2016-01-26 20:28:04 +0300

    entities project

--- Demo.DbContextMocking.Entities/Properties/AssemblyInfo.cs
+++ Demo.DbContextMocking.Entities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Demo.DbContextMocking")]
+[assembly: AssemblyTitle("Demo.DbContextMocking.Entities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Demo.DbContextMocking")]
+[assembly: AssemblyProduct("Demo.DbContextMocking.Entities")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
